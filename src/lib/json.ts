512e574212d32f0ff6774eda6ca240df816dd176
0xja-eth <804173948@qq.com>
2025-05-06 21:56:49 +0800

    fixbuild

--- src/lib/json.ts
+++ src/lib/json.ts
@@ -1,25 +1,25 @@
-function replacer(key, value) {
+function replacer(key: string, value: any) {
   if (typeof value === "bigint") {
     return value.toString() + "n";
   }
   return value;
 }
-function reviver(key, value) {
+function reviver(key: string, value: any) {
   if (typeof value === "string" && /^\d+n$/.test(value)) {
     return BigInt(value.slice(0, -1));
   }
   return value;
 }
-function numberReviver(key, value) {
+function numberReviver(key: string, value: any) {
   if (typeof value === "string" && /^\d+n$/.test(value)) {
     return Number(value.slice(0, -1));
   }
   return value;
 }
 
-export function data2Str(obj, space?: string | number) {
+export function data2Str(obj: any, space?: string | number) {
   return JSON.stringify(obj, replacer, space);
 }
-export function str2Data<T = any>(str, noBigInt = false): T {
+export function str2Data<T = any>(str: string, noBigInt = false): T {
   return JSON.parse(str, noBigInt ? numberReviver : reviver);
 }
